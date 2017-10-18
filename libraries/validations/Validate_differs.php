2e004a953870543cfc42af5e1ffd506054ca0253
Don Myers <donmyers@quadratec.com>
2017-10-18 13:52:18 -0400

    fix file

--- libraries/validations/Validate_differs.php
+++ libraries/validations/Validate_differs.php
@@ -15,6 +15,6 @@ class Validate_differs extends Validate_base {
 	public function validate(&$field, $options) {
 		$this->error_string = '%s must differ from %s.';
 
-		return !($this->field_data[$field] === $this->field_data[$options];
-		}
-	} /* end class */
+		return !($this->field_data[$field] === $this->field_data[$options]);
+	}
+} /* end class */
