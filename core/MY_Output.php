295b985a8b9b6e22cbb294ab6240e8e15d47282e
Don Myers <donmyers@quadratec.com>
2018-01-22 10:26:04 -0500

    refactor

--- core/MY_Output.php
+++ core/MY_Output.php
@@ -48,10 +48,11 @@ class MY_Output extends CI_Output {
 	public function nocache() {
 		log_message('debug', 'MY_Output::nocache');
 
-		$this->set_header('Expires: Sat,26 Jul 1997 05:00:00 GMT');
-		$this->set_header('Cache-Control: no-cache,no-store,must-revalidate,max-age=0');
-		$this->set_header('Cache-Control: post-check=0,pre-check=0', false);
-		$this->set_header('Pragma: no-cache');
+		$this
+			->set_header('Expires: Sat,26 Jul 1997 05:00:00 GMT')
+			->set_header('Cache-Control: no-cache,no-store,must-revalidate,max-age=0')
+			->set_header('Cache-Control: post-check=0,pre-check=0', false)
+			->set_header('Pragma: no-cache');
 
 		return $this;
 	}
