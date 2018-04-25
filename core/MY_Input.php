75df4d44a94bb2a9a359ac031d0772ad54248ee1
Don Myers <donmyers@quadratec.com>
2018-04-25 13:31:45 -0400

    clean up input again

--- core/MY_Input.php
+++ core/MY_Input.php
@@ -27,9 +27,16 @@ class MY_Input extends CI_Input {
 	protected $_request = [];
 
 	public function __construct() {
+		/* grab raw input for patch and such */
 		$this->_raw_input_stream = file_get_contents('php://input');
 
-		$this->_request = (empty($this->_raw_input_stream)) ? $_POST : $this->http_parse_query($this->_raw_input_stream);
+		/* try to parse the input */
+		parse_str($this->_raw_input_stream,$this->_request);
+
+		/* did we get anything? if not fall back to the posted input */
+		if (!count($this->_request)) {
+			$this->_request = $_POST;
+		}
 
 		/* call the parent classes constructor */
 		parent::__construct();
@@ -87,7 +94,7 @@ class MY_Input extends CI_Input {
 	 * @param array $remove array indexes - preformed last
 	 * @param string $default_model the name of the default model (where separator isn't present)
 	 * @param string $only_index only return this model
-	 * @param string $separator if this is present then treat each array index as a model + field pair
+	 * @param string $separator if this is present then treat each array index as a parent + child pair
 	 * @param boolean $append_model should "_model" be append to the array indexes
 	 * @param mixed $_request associated array index=>value - or - TRUE to use client input
 	 *
@@ -99,15 +106,17 @@ class MY_Input extends CI_Input {
 	 *
 	 * it is possible to append [] (brackets) after the field name in the
 	 * copy or move array in order to copy or move the value to the output of a array if needed
-	 * $copy = ['roles.parent_id[]'=>'id'];
+	 * $copy = ['roles|parent_id[]'=>'id'];
 	 * This would copy the id to each role index
 	 *
 	 */
-	public function request_remap($copy=[],$move=[],$remove=[],$default_model='#',$only_index=null,$separator='.',$append_model=false,$_request=[]) {
+	public function request_remap($copy=[],$move=[],$remove=[],$default_model='#',$only_index=null,$separator='|',$append_model=false,$_request=[]) {
 		/* use form request or what was sent in? */
 		if (is_bool($_request)) {
+			/* if it's true or false use the form request data */
 			$request = $this->_request;
 		} elseif (is_array($_request)) {
+			/* if it's a array then use what they sent in */
 			$request = $_request;
 		} else {
 			throw new Exception('Request Process input not an array or boolean.');
@@ -190,13 +199,14 @@ class MY_Input extends CI_Input {
 		/* do they want the entire group array or just a single part of it? */
 		$output_array = ($only_index) ? $groups[$only_index] : $groups;
 
-		/* default method output */
+		/* default method output is our "new" array */
 		$responds = $output_array;
 
 		/* if request argument was true then put back into internal request property */
 		if ($_request === true) {
 			$this->_request = $output_array;
 
+			/* switch the output to "this" to allow further chaining */
 			$responds = $this;
 		}
 
@@ -221,62 +231,6 @@ class MY_Input extends CI_Input {
 		$value = $this->_fetch_from_array($_COOKIE, $index, $xss_clean);
 
 		return ($value === null) ? $default : $value;
-	}
-
-	/*
-	http://php.net/manual/en/function.parse-str.php#119484
-	*/
-	protected function http_parse_query($queryString) {
-		$result = [];
-
-		if (!empty($queryString)) {
-			$parts = explode('&',$queryString);
-
-			foreach ($parts as $part) {
-				list($paramName, $paramValue) = explode('=',$part,2);
-
-				$paramName = urldecode($paramName);
-				$paramValue = urldecode($paramValue);
-
-				if (preg_match_all('/\[([^\]]*)\]/m', $paramName, $matches)) {
-					$paramName = substr($paramName, 0, strpos($paramName, '['));
-					$keys = array_merge([$paramName], $matches[1]);
-				} else {
-					$keys = [$paramName];
-				}
-
-				$target = &$result;
-
-				foreach ($keys as $index) {
-					if ($index === '') {
-						if (isset($target)) {
-							if (is_array($target)) {
-								$intKeys = array_filter(array_keys($target), 'is_int');
-								$index = count($intKeys) ? max($intKeys)+1 : 0;
-							} else {
-								$target = [$target];
-								$index  = 1;
-							}
-						} else {
-							$target = [];
-							$index = 0;
-						}
-					} elseif (isset($target[$index]) && !is_array($target[$index])) {
-						$target[$index] = [$target[$index]];
-					}
-
-					$target = &$target[$index];
-				}
-
-				if (is_array($target)) {
-					$target[] = $paramValue;
-				} else {
-					$target = $paramValue;
-				}
-			}
-		}
-
-		return $result;
 	}
 
 } /* end class */
