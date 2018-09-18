0e29e107e923343e5cc3bade317072f4949088e6
Don Myers <donmyers@icloud.com>
2018-09-18 14:51:32 -0400

    clean up comments

--- libraries/Page.php
+++ libraries/Page.php
@@ -33,25 +33,24 @@ class Page {
 	protected $page_variable_prefix;
 	protected $extending = false;
 
-/**
-* __construct
-* Insert description here
-*
-*
-* @return
-*
-* @access
-* @static
-* @throws
-* @example
-*/
+	/**
+	* __construct
+	* Insert description here
+	*
+	* @return
+	*
+	* @access
+	* @static
+	* @throws
+	* @example
+	*/
 	public function __construct(&$config=[]) {
 		$this->config = &$config;
 
 		$this->load = &ci('load');
 		$this->output = &ci('output');
 		$this->event = &ci('event');
-		
+
 		/* if it's true then use the default else use what's in page_min config */
 		define('PAGE_MIN',(($this->config['page_min'] === true) ? '.min' : $this->config['page_min']));
 
@@ -70,74 +69,85 @@ class Page {
 		log_message('info', 'Page Class Initialized');
 	}
 
+	/**
+	* route
+	* Insert description here
+	*
+	* @param $route
+	*
+	* @return $this
+	*
+	*/
 	public function route($route='') {
-		$this->route = $route;
+		$this->route = str_replace('-', '_',$route);
 
 		return $this;
 	}
 
-/**
-* title
-* Insert description here
-*
-* @param $title
-*
-* @return $this
-*
-*/
+	/**
+	* title
+	* Insert description here
+	*
+	* @param $title
+	*
+	* @return $this
+	*
+	*/
 	public function title($title = '',$priority = 50) {
 		return $this->add('title',$title,$priority);
 	}
 
-/**
-* meta
-* Insert description here
-*
-* @param $attr
-* @param $name
-* @param $content
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* meta
+	* Insert description here
+	*
+	* @param $attr
+	* @param $name
+	* @param $content
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function meta($attr, $name, $content = null,$priority = 50) {
 		return $this->add('meta','<meta '.$attr.'="'.$name.'"'.(($content) ? ' content="'.$content.'"' : '').'>'.PHP_EOL,$priority);
 	}
 
-/**
-* body_class
-* Insert description here
-*
-* @param $class
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* body_class
+	* Insert description here
+	*
+	* @param $class
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function body_class($class,$priority = 50) {
 		return (is_array($class)) ? $this->_body_class($class,$priority) : $this->_body_class(explode(' ',$class));
 	}
-	
-/**
-* render
-* Insert description here
-*
-* @param $view string
-* @param $data array
-*
-* @return $this
-*
-*/
-	public function render($view = null, $data = []) {
+
+	/**
+	* render
+	* Insert description here
+	*
+	* @param $view string
+	* @param $data array
+	*
+	* @return $this
+	*
+	*/
+	public function render($view = null, $data = null) {
 		log_message('debug', 'page::render::'.$view);
 
-		$view = ($view) ? $view : str_replace('-', '_',$this->route);
+		$view = ($view) ?? $this->route;
 
 		$this->event->trigger('page.render',$this,$view);
 		$this->event->trigger('page.render.'.str_replace('/','.',$view),$this,$view);
 
-		$this->data($data);
+		if (is_array($data)) {
+			$this->data($data);
+		}
 
 		/* this is going to be the "main" section */
 		$view_content = $this->view($view);
@@ -152,17 +162,18 @@ class Page {
 
 		return $this;
 	}
-/**
-* view
-* Insert description here
-*
-* @param $_view_file string
-* @param $_data array
-* @param $_return mixed
-*
-* @return mixed
-*
-*/
+
+	/**
+	* view
+	* Insert description here
+	*
+	* @param $_view_file string
+	* @param $_data array
+	* @param $_return mixed
+	*
+	* @return mixed
+	*
+	*/
 	public function view($_view_file = null, $_data = [], $_return = true) {
 		/* call core orange function view() */
 		$_buffer = view($_view_file,array_merge($this->load->get_vars(),(array)$_data));
@@ -174,16 +185,16 @@ class Page {
 		return ($_return === true) ? $_buffer : $this;
 	}
 
-/**
-* data
-* Insert description here
-*
-* @param $name
-* @param $value
-*
-* @return $this
-*
-*/
+	/**
+	* data
+	* wrapper
+	*
+	* @param $name
+	* @param $value
+	*
+	* @return $this
+	*
+	*/
 	public function data($name = null, $value = null) {
 		$this->load->vars($name,$value);
 
@@ -191,6 +202,15 @@ class Page {
 	}
 
 
+	/**
+	* extended
+	* Insert description here
+	*
+	* @param $template
+	*
+	* @return $this
+	*
+	*/
 	public function extend($template=null) {
 		if ($this->extending) {
 			throw new Exception('You are already extending "'.$this->extending.'" therefore we cannot extend "'.$name.'".');
@@ -201,16 +221,16 @@ class Page {
 		return $this;
 	}
 
-/**
-* css
-* Insert description here
-*
-* @param $file
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* css
+	* Insert description here
+	*
+	* @param $file
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function css($file = '',$priority = 50) {
 		if (is_array($file)) {
 			foreach ($file as $f) {
@@ -222,43 +242,43 @@ class Page {
 		return $this->add('css',$this->link_html($file).PHP_EOL,$priority);
 	}
 
-/**
-* link_html
-* Insert description here
-*
-* @param $file
-*
-* @return string
-*
-*/
+	/**
+	* link_html
+	* Insert description here
+	*
+	* @param $file
+	*
+	* @return string
+	*
+	*/
 	public function link_html($file) {
 		return $this->ary2element('link', array_merge($this->config['link_attributes'],['href' => $file]));
 	}
 
-/**
-* style
-* Insert description here
-*
-* @param $style
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* style
+	* Insert description here
+	*
+	* @param $style
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function style($style,$priority = 50) {
 		return $this->add('style',$style.PHP_EOL,$priority);
 	}
 
-/**
-* js
-* Insert description here
-*
-* @param $file
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* js
+	* Insert description here
+	*
+	* @param $file
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function js($file = '',$priority = 50) {
 		if (is_array($file)) {
 			foreach ($file as $f) {
@@ -270,30 +290,30 @@ class Page {
 		return $this->add('js',$this->script_html($file).PHP_EOL,$priority);
 	}
 
-/**
-* script_html
-* Insert description here
-*
-* @param $file
-*
-* @return string
-*
-*/
+	/**
+	* script_html
+	* Insert description here
+	*
+	* @param $file
+	*
+	* @return string
+	*
+	*/
 	public function script_html($file) {
 		return $this->ary2element('script', array_merge($this->config['script_attributes'], ['src' => $file]),'');
 	}
 
-/**
-* js_variable
-* Insert description here
-*
-* @param $key
-* @param $value
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* js_variable
+	* Insert description here
+	*
+	* @param $key
+	* @param $value
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function js_variable($key,$value,$priority = 50,$raw=false) {
 		if ($raw) {
 			$value = 'var '.$key.'='.$value.';' ;
@@ -304,15 +324,15 @@ class Page {
 		return $this->add('js_variables',$value,$priority);
 	}
 
-/**
-* js_variables
-* Insert description here
-*
-* @param $array
-*
-* @return $this
-*
-*/
+	/**
+	* js_variables
+	* Insert description here
+	*
+	* @param $array
+	*
+	* @return $this
+	*
+	*/
 	public function js_variables($array) {
 		foreach ($array as $k => $v) {
 			$this->js_variable($k, $v);
@@ -321,68 +341,84 @@ class Page {
 		return $this;
 	}
 
-/**
-* script
-* Insert description here
-*
-* @param $script
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* script
+	* Insert description here
+	*
+	* @param $script
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function script($script,$priority = 50) {
 		return $this->add('script',$script.PHP_EOL,$priority);
 	}
 
-/**
-* domready
-* Insert description here
-*
-* @param $script
-* @param $priority integer
-*
-* @return $this
-*
-*/
+	/**
+	* domready
+	* Insert description here
+	*
+	* @param $script
+	* @param $priority integer
+	*
+	* @return $this
+	*
+	*/
 	public function domready($script,$priority = 50) {
 		return $this->add('domready',$script.PHP_EOL,$priority);
 	}
 
-	public function tag($name,$attributes,$priority = 50) {
-		return $this->add($name,$this->ary2element($name,$attributes),$priority);
-	}
+	/**
+	* tag
+	* Insert description here
+	*
+	* Unpaired html tag
+	* tag('link',['rel'=>'icon','type'=>'image/x-icon','href'=>'/asset/image.jpg'],50)
+	* <link rel="icon" type="image/x-icon" href="/asset/image.jpg"/>
+	*
+	* Paired html tag w/content
+	* element('p',['class'=>'highlight','id'=>'pid3'],'This is important!',50)
+	* <p class="highlight" id="pid3">This is important!</p>
+	*
+	*/
+	public function tag($name,$attributes,$content = false,$priority = 50) {
+		if (func_num_args() == 3 && is_integer($content)) {
+			$priority = $content;
+			$content = false;
+		}
 
-	public function element($name,$attributes,$content,$priority = 50) {
 		return $this->add($name,$this->ary2element($name,$attributes,$content),$priority);
 	}
 
-/**
-* ary2element
-*
-* @param $element
-* @param $attributes
-* @param $wrapper
-*
-* @return string
-*
-*/
+	/**
+	* ary2element
+	* Insert description here
+	*
+	* @param $element
+	* @param $attributes
+	* @param $wrapper
+	*
+	* @return string
+	*
+	*/
 	public function ary2element($element, $attributes, $wrapper = false) {
 		$output = '<'.$element._stringify_attributes($attributes);
 
 		return ($wrapper === false) ? $output.'/>' : $output.'>'.$wrapper.'</'.$element.'>';
 	}
 
-/**
-* add element
-*
-* @param $name
-* @param $value
-* @param $priority
-*
-* @return $this
-*
-*/
+	/**
+	* add element
+	* Insert description here
+	*
+	* @param $name
+	* @param $value
+	* @param $priority (-100 to 100)
+	*
+	* @return $this
+	*
+	*/
 	public function add($name,$value,$priority=50,$prevent_duplicates=true) {
 		$key = md5($value);
 
@@ -394,7 +430,18 @@ class Page {
 
 		return $this;
 	}
-	
+
+	/**
+	* var
+	* retrieve a page variable (with "post" processing)
+	* title, meta, body_class, css, style, js, script, js_variables, script, domready
+	* any additional "tags"
+	*
+	* @param $name
+	*
+	* @return string
+	*
+	*/
 	public function var($name) {
 		return $this->_prepare_page_variable($this->variables[$name],$this->load->get_var($this->page_variable_prefix.$name));
 	}
@@ -404,7 +451,7 @@ class Page {
 	protected function _prepare_page_variable($priority_queue,$content) {
 		if (is_array($priority_queue)) {
 			ksort($priority_queue);
-	
+
 			/* add the currently available entries */
 			foreach ($priority_queue as $priority) {
 				foreach ($priority as $string) {
@@ -420,7 +467,7 @@ class Page {
 		foreach ($class as $c) {
 			$this->add('body_class',strtolower($c).' ',$priority);
 		}
-		
+
 		return $this;
 	}
 
