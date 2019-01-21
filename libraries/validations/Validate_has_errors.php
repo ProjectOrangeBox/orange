aceeb16112efd234b51a5e9533e54af5cdd1e01a
Don Myers <donmyers@icloud.com>
2019-01-21 15:36:20 -0500

    new has errors validation to stop further rule processing

---
+++ libraries/validations/Validate_has_errors.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Validate_alpha_dash
+ * Insert description here
+ *
+ * @package CodeIgniter / Orange
+ * @author Don Myers
+ * @copyright 2018
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @link https://github.com/ProjectOrangeBox
+ * @version 2.0
+ *
+ * required
+ * core:
+ * libraries:
+ * models:
+ * helpers:
+ * functions:
+ * @help validate a error hasn't already been returned has_errors[group,field] has_errors[group] used to stop further rule processing in piped chain
+ *
+ */
+class Validate_has_errors extends Validate_base {
+
+	public function validate(&$field, $options) {
+		if (strpos($options,',')) {
+			list($group,$field) = explode(',',$options);
+
+			$errors = ci('errors')->as_array($group);
+
+			$does_not_have_error = !isset($errors[$field]);
+		} else {
+			$does_not_have_error = !ci('errors')->has($options);
+		}
+
+		return (bool)$does_not_have_error;
+	}
+}
