3e911a456aacf730364ce2986d421fac37b8ba47
Don Myers <donmyers@icloud.com>
2019-02-15 15:00:04 -0500

    add default controller and view

---
+++ support/build/controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+class MainController extends MY_Controller
+{
+	public function indexAction()
+	{
+		$this->page->render();
+	}
+
+	public function route404Action()
+	{
+		show_404();
+	}
+} /* end class */
