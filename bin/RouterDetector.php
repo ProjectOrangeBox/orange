277eb792adf36f964862b5d2eec7741d821b8212
Don Myers <donmyers@me.com>
2026-07-18 00:00:07 -0400

    clean up bin helpers

--- bin/RouterDetector.php
+++ bin/RouterDetector.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+namespace config;
+
+use ReflectionClass;
+use ReflectionMethod;
 use orange\framework\attributes\Route;
 
 /**
--- bin/psr12/runFixPSR12.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# quick script to call the code sniffer. By default it will do the whole lib directory
-
-../../../../vendor/squizlabs/php_codesniffer/bin/phpcbf --standard=PSR12 --exclude=Generic.Files.LineLength --colors ../../src
